03c7bed12930f21a7163a93b2489e74215238f8b
PuChin <puching110@gmail.com>
2014-06-20 10:34:31 +0800

    url + title + content + timeout=1sec

--- crawler.js
+++ crawler.js
@@ -2,13 +2,17 @@ var request = require('request'),
 	cheerio = require('cheerio'),
 	fs = require('fs'),
 	moment = require('moment'),
-	results = [], check = [],
-	pages = 2, index = 1, test = [];
+	results = [], check = [], flag = [], query = [],
+	pages = 2, index = 1, argc, errNum = 0, saved = 0;
 
+process.argv.forEach(function(val, index, array) {
+	if (val === '-p') pages = array[index+1];
+	else if (val === '-q') {
+	}
+});
 
-var flag = [];
-for (var i = 0; i < pages; i++) flag.push(false);
 function req(i) {
+	console.log('page:'+i);
 	var url = 'https://www.google.com.tw/search?q=web+development+tutorial&es_sm=91&ei=TI2iU6aJK4jMkwXp04FQ&start=' + (i*10).toString() + '&sa=N&biw=1015&bih=503';
 	request(url, function (error, response, body) {
 		if (error || response.statusCode !== 200) {
@@ -18,29 +22,56 @@ function req(i) {
 
 		$('.g').each(function(iter, elem) {
 			// purify the url
-			var item = {"url":""};
-			var regex=/&sa=.*/gi;
+			var item = {"url":"","title":"","content":""};
 			var str = $(this).find('.r a').attr('href'),
 				res = str.replace("/url?q=", ""),
 				title = $(this).find('h3').text();
 
-			item.url = res.replace(regex, "");
+			item.url = res.replace(/&sa=.*/gi, "");
 			item.title = title;
 			results.push(item);
 
 		});
-		if (check.length === pages-1) 
-			save();
-		else 
+
+
+		//non-blocking
+		if (check.length === pages-1) {
+			for (var j = 0; j < results.length; j++)
+				getContent(j);
+		}
+		else {
 			check.push(true);
+			setTimeout(function(){req(i+1)}, 1000);
+		}
 	});
 }
 
 function save(){
+	saved = 1;
 	var data = JSON.stringify(results, null, "\t");
 	fs.writeFileSync('results.json', data);
 }
 
-for (var i = 0; i < pages; i++) {
-	req(i);
+function getContent(i){
+	var url = results[i].url;
+
+	request(url, function (error, response, body) {
+		if (error || response.statusCode !== 200) {
+			errNum++;
+	    	return;
+	  	}
+		$ = cheerio.load(body);
+		// results[i].content = $('body').text();
+		results[i].content = $('body').text().replace(/[\r\t\n]/gi, "");
+
+		//non-blocking
+		if (flag.length === results.length-1 - errNum) {
+			save();
+		}
+		else {
+			flag.push(true);
+		}
+	});
 }
+
+setTimeout(function(){req(0)}, 1000);
