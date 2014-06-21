5772962dbc1f25528353c2fac33104652b9ee75a
Ray Shih <mnf.shih@gmail.com>
2014-06-22 00:32:08 +0800

    crawler now can run more then one level

--- crawler.js
+++ crawler.js
@@ -1,77 +1,148 @@
-var request = require('request'),
-	cheerio = require('cheerio'),
-	fs = require('fs'),
-	moment = require('moment'),
-	results = [], check = [], flag = [], query = [],
-	pages = 2, index = 1, argc, errNum = 0, saved = 0, time = 5000;
+var request = require('request');
+var async = require('async');
+var	cheerio = require('cheerio');
+var fs = require('fs');
 
-process.argv.forEach(function(val, index, array) {
-	if (val === '-p') pages = array[index+1];
-	else if (val === '-t') time = parseInt(array[index+1])*1000;
-	else {
+var googleUrl = function(query, page){
+  var queryString = query.replace(/\s+/g, '+');
+  var start = page * 10;
+  return 'https://www.google.com.tw/search?q=' + queryString + '&start=' + start;
+};
 
-	}
+var parseGoogleResult = function (body, cb){
+  $ = cheerio.load(body);
+  var items = $('.g').map(function (iter, elem){
+    var rawUrl = $(this).find('.r a').attr('href');
+
+    return {
+      url : rawUrl.replace("/url?q=", "").replace(/&sa=.*/gi, ""),
+      title : $(this).find('h3').text()
+    };
+  }).toArray();
+
+  cb(null, items);
+};
+
+var queryGoogle = function (query, page, cb){
+	var url = googleUrl(query, page);
+	request(url, function(error, response, body){
+		if (error || response.statusCode !== 200)
+      return cb(new Error('query fail: ' + url));
+
+    parseGoogleResult(body, cb);
+	});
+};
+
+var removeHash = function (url){
+  return url.replace(/#.*$/, '');
+};
+
+var extractLinks = function(url, body, cb){
+  var root = url.match(/^https?:\/\/[^\/]+/)[0] + '/';
+
+  $ = cheerio.load(body);
+  async.map($('a[href]').toArray(), function(item, cb){
+    var href = $(item).attr('href');
+    if (href.match(/^javascript/)) return cb();
+
+    var url = removeHash(href);
+    if (!url.match(/^http/)){
+      url = root + url.replace(/^\//, '');
+    }
+
+    cb(null, url);
+  }, cb);
+};
+
+var crawl = function (url, cb){
+  if(!url) return cb();
+
+  url = removeHash(url);
+  if (crawled.indexOf(url) != -1) return cb();
+
+  console.log('crawl url: ' + url);
+
+  crawled.push(url);
+  request(url, function (err, res, body){
+    if(err || res.statusCode != 200) return cb();
+    console.log('crawl url: ' + url + ' done');
+
+    async.waterfall([
+      function(cb){
+        extractLinks(url, body, function(err, targetUrls){
+          if(err) return cb(err);
+
+          targetUrls = targetUrls.filter( function(url){
+            if(!url) return false;
+
+            return !url.match(/(jpg|png|gif|zip)$/i);
+          });
+
+          [].push.apply(urlList, targetUrls);
+
+          cb(null, targetUrls);
+        });
+      }, 
+      function(urls, cb){
+        var item = {
+          title: $(body).find('h1').first().text(),
+          content: body.replace(/[\r\n\t]+/g, ' '),
+          links: urls
+        };
+
+        var json = JSON.stringify(item);
+        fs.appendFile('data/results.jl', json + '\n', cb);
+      }
+    ], cb);
+  });
+};
+
+var crawled = []; // TODO append to files
+var urlList = [];
+var queryString = 'web development tutorial';
+var totalLevel = 2;
+var concurrent = 20;
+var pages = 2;
+var time = 5000;
+
+process.argv.forEach(function(key, index, array) {
+  var value = array[index + 1];
+  switch(key){
+    case '-p':
+      pages = value;
+      break;
+    case '-t':
+      time = parseInt(value, 10)*1000;
+      break;
+    case '-l':
+      totalLevel = parseInt(value, 10);
+      break;
+  }
 });
 
+async.series([
+  function (cb){
+    async.timesSeries(pages, function (n, next){
+      console.log('query google page: ' + n + '...');
+      queryGoogle(queryString, n, next);
+    }, function(err, results){
+      if (err) return cb(err);
+      var flatArr = [].concat.apply([], results);
+      [].push.apply(urlList, flatArr.map(function (item){ return item.url; }));
+      cb(null);
+    });
+  }, function (cb){
+    async.timesSeries(totalLevel, function (level, next){
+      console.log('level: ' + level + ', number of url: ' + urlList.length);
 
-function req(i) {
-	// console.log('page:'+i);
-	var url = 'https://www.google.com.tw/search?q=web+development+tutorial&es_sm=91&ei=TI2iU6aJK4jMkwXp04FQ&start=' + (i*10).toString() + '&sa=N&biw=1015&bih=503';
-	request(url, function (error, response, body) {
-		if (error || response.statusCode !== 200) return;
+      var currentUrlList = urlList;
+      urlList = [];
 
-		$ = cheerio.load(body);
+      async.eachLimit(currentUrlList, concurrent, crawl, next);
+    }, cb);
+  }
+], function(err){
+  console.log('done');
+  console.log('total ' + crawled.length + ' pages crawled ');
+});
 
-		$('.g').each(function(iter, elem) {
-			// purify the url
-			var item = {"url":"","title":"","content":""};
-			var str = $(this).find('.r a').attr('href'),
-				res = str.replace("/url?q=", ""),
-				title = $(this).find('h3').text();
-
-			item.url = res.replace(/&sa=.*/gi, "");
-			item.title = title;
-			results.push(item);
-		});
-
-		//non-blocking
-		if (check.length === pages-1) {
-			for (var j = 0; j < results.length; j++)
-				getContent(j);
-		}
-		else {
-			check.push(true);
-			setTimeout(function(){req(i+1);}, time);
-		}
-	});
-}
-
-function save(){
-	saved = 1;
-	var data = JSON.stringify(results, null, "\t");
-	fs.writeFileSync('data/results.json', data);
-}
-
-function getContent(i){
-	var url = results[i].url;
-
-	request(url, function (error, response, body) {
-		if (error || response.statusCode !== 200) {
-			errNum++;
-	    	return;
-	  	}
-		$ = cheerio.load(body);
-		// results[i].content = $('body').text();
-		results[i].content = $('body').text().replace(/[\r\t\n]/gi, "");
-
-		//non-blocking
-		if (flag.length === results.length-1 - errNum) {
-			save();
-		}
-		else {
-			flag.push(true);
-		}
-	});
-}
-
-setTimeout(function(){req(0);}, time);
